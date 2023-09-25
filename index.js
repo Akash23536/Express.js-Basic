6a472e8293203c77d41666d959bf293662f769a3
Akash Bhadana <23536akash.2021@gmail.com>
2023-09-25 16:28:23 +0530

    body-parser done

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-// [npm i express]  in terminal to install express 
-const express = require("express");  // importing express.js 
-const path = require("path"); // importing path to use  sendFile() method
+const express = require("express");  // npm i express 
+const path = require("path"); // importing path to use sendFile() 
+const bodyParser = require("body-parser") // npm i body-parser to use req.body
 
 const app = express();  // express.   instead of calling express. 
 const port = 4000; // defining port at which app.listen
+app.use(bodyParser.urlencoded({ extended: false })); // using body-parser as a middleware=> always define it after the app 
 
 //4 methods of http => GET - read; POST - create; PUT - update; DELETE  - delete
 
-app.get("/", (req, res) => { // by default home page
-    res.sendFile(path.join(__dirname+"/login.html"))  // using sendFile("./login") is not a good practise use path
+app.get("/", (req, res) => {                             // "/" is default page or landing page or home page
+    res.sendFile(path.join(__dirname + "/login.html"))  // using sendFile("./login") is not a good practise use path
 })
 
-app.post("/login",(req,res)=>{
-    res.send("<h1>done</h1>")
-    console.log("name")
-    console.log(req.body)
+app.post("/login", (req, res) => {
+    res.send(`<h1>Done Mr ${req.body.name}</h1> <h2> your Email is ${req.body.email}</h2>`) //send response in <h1> and <h2> tag
+    console.log(req.body) // print body object with name,email,password with the help of body-parser
 })
-
 
 app.listen(port, () => {
     console.log(`server is working on port: ${port}`)
 });
 
-// node ./index.js  to run the server every time
+// node ./index.js  to run the server or file
 // ctrl +c          to close the server
 
 //nodemon index.js   to solve the above issue 
