ac3f19e62e5ff2c151d5cdc5b4c5cd116c39b5dd
Akash Bhadana <23536akash.2021@gmail.com>
2023-09-25 15:57:30 +0530

    html page added and post  body parser topic

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 // [npm i express]  in terminal to install express 
-const express = require("express");  // importing express 
+const express = require("express");  // importing express.js 
+const path = require("path"); // importing path to use  sendFile() method
 
 const app = express();  // express.   instead of calling express. 
 const port = 4000; // defining port at which app.listen
 
 //4 methods of http => GET - read; POST - create; PUT - update; DELETE  - delete
 
-
 app.get("/", (req, res) => { // by default home page
-    res.send("<h1> hello world response</h1>")
+    res.sendFile(path.join(__dirname+"/login.html"))  // using sendFile("./login") is not a good practise use path
 })
-app.get("/about", (req, res) => {
-    res.send("<h1>about page response</h1>")
+
+app.post("/login",(req,res)=>{
+    res.send("<h1>done</h1>")
+    console.log("name")
+    console.log(req.body)
 })
-app.get("/contact", (req, res) => {
-    res.send("<h1> contact page response</h1>")
-})
+
 
 app.listen(port, () => {
     console.log(`server is working on port: ${port}`)
 });
+
 // node ./index.js  to run the server every time
 // ctrl +c          to close the server
 
